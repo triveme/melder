9d48f8f133e7a6d076f1d06973826cc8e1db4504
MarvinHartmann <marvin.hartmann@edag.com>
2023-06-21 11:16:30 +0200

    Improve handling of image upload and error handling, add new report category 'lamps'

--- backend/routes/defect.routes.js
+++ backend/routes/defect.routes.js
@@ -5,7 +5,17 @@ var express = require("express");
 var defectRouter = express.Router();
 var xmlParser = require("express-xml-bodyparser");
 
-defectRouter.post("/", [storeImage.array("file", 3)], controller.reportDefect);
+const fileSizeLimitErrorHandler = (err, req, res, next) => {
+  if (err) {    
+    res.status(400).send({
+      message: "Fehler: Bilder sind zu groß",
+    });
+  } else {
+    next()
+  }
+}
+
+defectRouter.post("/", storeImage.array("file", 3),fileSizeLimitErrorHandler, controller.reportDefect);
 
 defectRouter.post(
   "/external",
